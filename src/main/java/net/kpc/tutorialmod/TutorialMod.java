e61e0db21b00da6027f2a81659b2fdbcb24d8f09
KP Cunningham <mackin153@hotmail.com>
2025-01-14 02:59:55 +0000

    Add custom item

--- src/main/java/net/kpc/tutorialmod/TutorialMod.java
+++ src/main/java/net/kpc/tutorialmod/TutorialMod.java
@@ -1,6 +1,9 @@
 package net.kpc.tutorialmod;
 
 import com.mojang.logging.LogUtils;
+import net.kpc.tutorialmod.item.ModItems;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -32,6 +35,8 @@ public class TutorialMod
 
         MinecraftForge.EVENT_BUS.register(this);
 
+        ModItems.register(modEventBus);
+
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
         // Register our mod's ForgeConfigSpec so that Forge can create and load the config file for us
@@ -46,7 +51,10 @@ public class TutorialMod
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event)
     {
-
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.ALEXANDRITE);
+            event.accept(ModItems.RAW_ALEXANDRITE);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
